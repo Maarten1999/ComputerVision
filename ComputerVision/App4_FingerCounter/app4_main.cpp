fbfb81baa61f7f1daa834e7abca762e2e9261ebc
Dion <dionvanderlinden@live.nl>
2019-06-06 14:19:37 +0200

    Sicko mode

--- ComputerVision/App4_FingerCounter/app4_main.cpp
+++ ComputerVision/App4_FingerCounter/app4_main.cpp
@@ -22,6 +22,52 @@ typedef cv::Ptr<cv::BackgroundSubtractorMOG2> MOG2Ptr;
 
 Mat subMask(MOG2Ptr sub, const Mat& frame);
 
+float innerAngle(float px1, float py1, float px2, float py2, float cx1, float cy1)
+{
+
+	float dist1 = std::sqrt((px1 - cx1)*(px1 - cx1) + (py1 - cy1)*(py1 - cy1));
+	float dist2 = std::sqrt((px2 - cx1)*(px2 - cx1) + (py2 - cy1)*(py2 - cy1));
+
+	float Ax, Ay;
+	float Bx, By;
+	float Cx, Cy;
+
+	//find closest point to C  
+	//printf("dist = %lf %lf\n", dist1, dist2);  
+
+	Cx = cx1;
+	Cy = cy1;
+	if (dist1 < dist2)
+	{
+		Bx = px1;
+		By = py1;
+		Ax = px2;
+		Ay = py2;
+
+
+	}
+	else {
+		Bx = px2;
+		By = py2;
+		Ax = px1;
+		Ay = py1;
+	}
+
+
+	float Q1 = Cx - Ax;
+	float Q2 = Cy - Ay;
+	float P1 = Bx - Ax;
+	float P2 = By - Ay;
+
+
+	float A = std::acos((P1*Q1 + P2 * Q2) / (std::sqrt(P1*P1 + P2 * P2) * std::sqrt(Q1*Q1 + Q2 * Q2)));
+
+	A = A * 180 / CV_PI;
+
+	return A;
+}
+
+
 int main(int argc, char** argv)
 {
 	VideoCapture cap(1);
@@ -38,6 +84,7 @@ int main(int argc, char** argv)
 
 	MOG2Ptr subtractor;
 	//subtractor = cv::createBackgroundSubtractorMOG2(10, 30, false);
+	int inAngleMin = 200, inAngleMax = 300, angleMin = 180, angleMax = 359, lengthMin = 10, lengthMax = 80;
 
 	cv::namedWindow(WINDOW_NAME);
 	int minH = 0, maxH = 73, minS = 0, maxS = 201, minV = 16, maxV = 255;
@@ -82,9 +129,51 @@ int main(int argc, char** argv)
 		// Convex hull
 		if (!contours.empty())
 		{
+			
 			std::vector<std::vector<cv::Point> > hull(1);
-			cv::convexHull(cv::Mat(contours[largestContour]), hull[0], false);
+			Mat largestContourMat = cv::Mat(contours[largestContour]);
+			cv::convexHull(largestContourMat, hull[0], false);
 			cv::drawContours(frame, hull, 0, cv::Scalar(66, 116, 244), 3);
+
+			if (hull[0].size() > 2)
+			{
+				vector<cv::Vec4i> convexityDefects;
+				vector<int> hullIndexes;
+				cv::convexHull(largestContourMat, hullIndexes, true);
+				cv::convexityDefects(largestContourMat, hullIndexes, convexityDefects);
+
+				cv::Rect boundingBox = cv::boundingRect(hull[0]);
+				cv::rectangle(frame, boundingBox, cv::Scalar(255, 0, 0));
+				cv::Point center = cv::Point(boundingBox.x + boundingBox.width / 2, boundingBox.y + boundingBox.height / 2);
+
+				vector<cv::Point> fingers;
+				for (int i = 0; i < convexityDefects.size(); i++)
+				{
+					cv::Point p1 = contours[largestContour][convexityDefects[i][0]];
+					cv::Point p2 = contours[largestContour][convexityDefects[i][1]];
+					cv::Point p3 = contours[largestContour][convexityDefects[i][2]];
+					cv::line(frame, p1, p3, cv::Scalar(0, 0, 255), 2);
+					cv::line(frame, p3, p2, cv::Scalar(0, 0, 255), 2);
+
+					double angle = std::atan2(center.y - p1.y, center.x - p1.x) * 180 / CV_PI;
+					double inAngle = innerAngle(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
+					double length = std::sqrt(std::pow(p1.x - p3.x, 2) + std::pow(p1.y - p3.y, 2));
+					if (angle > angleMin - 180 && angle < angleMax - 180 && inAngle > inAngleMin - 180 && inAngle < inAngleMax - 180 && length > lengthMin / 100.0 * boundingBox.height && length < lengthMax / 100.0 * boundingBox.height)
+					{
+						fingers.push_back(p1);
+					}
+				
+				}
+
+				for (size_t i = 0; i < fingers.size(); i++)
+				{
+					cv::circle(frame, fingers[i], 9, cv::Scalar(0, 255, 0), 2);
+				}
+
+				std::string text = "Fingers: " + fingers.size();
+				cv::putText(frame, text, cv::Point(20, 30), CV_FONT_NORMAL, 0.8, cv::Scalar(255, 255, 255));
+
+			}
 		}
 
 		cv::imshow(WINDOW_NAME, frame);
