cb2b93c67c4d4436092096bd956b59887b46cbf0
Dion <dionvanderlinden@live.nl>
2019-06-06 10:46:30 +0200

    [Added] background subtraction

--- ComputerVision/App4_FingerCounter/app4_main.cpp
+++ ComputerVision/App4_FingerCounter/app4_main.cpp
@@ -1,6 +1,6 @@
 #include <opencv2/opencv.hpp>
-#include "opencv2/imgproc/imgproc.hpp" 
-#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/imgproc/imgproc.hpp> 
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -16,6 +16,10 @@ using std::vector;
 using std::string;
 
 #define ESCAPE_KEY 27
+
+typedef cv::Ptr<cv::BackgroundSubtractorMOG2> MOG2Ptr;
+
+Mat subMask(MOG2Ptr sub, const Mat& frame);
 
 int main(int argc, char** argv)
 {
@@ -38,18 +42,39 @@ int main(int argc, char** argv)
 
 	const auto element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5));
 
+	MOG2Ptr subtractor;
+	subtractor = cv::createBackgroundSubtractorMOG2(10, 30, false);
 	while (cv::waitKey(1) != ESCAPE_KEY)
 	{
 		std::vector<cv::Mat> contours;
 		vector<cv::Vec4i> hierarchy;
 
-		if (cap.read(frame))
+		if (!cap.read(frame))
 		{
-			
-
+			cout << "Failed to read frame\n"; 
+			break;
 		}
+		Mat res = subMask(subtractor, frame);
+		
+		cv::imshow("WINDOW", res);
+
 	}
 	return 0;
+}
+
+Mat subMask(MOG2Ptr sub, const Mat& frame)
+{
+	Mat mask;
+	sub->apply(frame, mask, 0);
+	
+	cv::MatExpr kernel = Mat::ones(cv::Size(4, 4), CV_8U);
+	cv::morphologyEx(mask, mask, cv::MORPH_OPEN, kernel, cv::Point(-1,-1), 2);
+	cv::morphologyEx(mask, mask, cv::MORPH_CLOSE, kernel, cv::Point(-1,-1), 2);
+
+	Mat result;
+	cv::bitwise_and(frame, frame, result, mask);
+	
+	return result;
 }
 
 
